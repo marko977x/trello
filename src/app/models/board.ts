f8ee92555529f3179b24f68bcb0277dac9e3c669
Marko Arandjelovic <marko977x@gmail.com>
2019-06-19 19:42:46 +0200

    Add models

---
+++ src/app/models/board.ts
@@ -0,0 +1,6 @@
+export interface Board {
+  id: string,
+  title: string,
+  lists: string[],
+  userId: string
+}
---
+++ src/app/models/list.ts
@@ -0,0 +1,6 @@
+export interface List {
+  id: string,
+  title: string,
+  boardId: string,
+  cards: string[]
+}
